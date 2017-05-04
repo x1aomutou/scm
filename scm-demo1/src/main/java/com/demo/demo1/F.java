e0824b319ed5131d1a7218467b091b5239874599
lzy <lzy@qq.xom>
2017-05-04 17:35:43 +0800

    v1

---
+++ scm-demo1/src/main/java/com/demo/demo1/F.java
@@ -0,0 +1,19 @@
+package com.demo.demo1;
+/**
+ * 
+ * @author liu
+ * @Date :2017-05-03
+ * @version v-1.0
+ * @since v -1.0
+ *
+ */
+public class F {
+	/**
+	 * print hello world
+	 * @return 1
+	 */
+	public int print(){
+		System.out.println("Hello World!");
+		return 1;
+	}
+}
---
+++ scm-demo1/src/test/java/demo1/com/demo/demo1/test/TestF.java
@@ -0,0 +1,30 @@
+package demo1.com.demo.demo1.test;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.demo.demo1.F;
+/**
+ * 
+ * @author liu
+ * @Date :2017-05-03
+ * @version v-1.0
+ * @since v -1.0
+ * F test
+ *
+ */
+public class TestF {
+	private static F f=null;
+	@Before
+	public void init(){
+		f=new F();
+		
+	}
+	@Test
+	public void testPrint() {
+		int version=f.print();
+		Assert.assertTrue(version==1);
+	}
+
+}
